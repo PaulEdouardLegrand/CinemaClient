c5176dfa596cec9000b3230324b4875411711760
Paul <pauledouardlegrand@gmail.com>
2021-01-26 12:50:01 +0100

    final web application for the "cinema" project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
+<script src="https://unpkg.com/vue/dist/vue.js"></script>
+<script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Bienvenue dans notre application !</h1>
+      <!-- utilisez le composant router-link pour la navigation. -->
+      <!-- spécifiez le lien en le passant à la prop `to` -->
+      <!-- `<router-link>` sera rendu en tag `<a>` par défaut -->
+      <div class = "links">
+        <router-link to="/films">Les Films</router-link>
+            
+        <router-link to="/acteurs">Les Acteurs</router-link>
+      </div>
+
+    <!-- balise pour le composant router-view -->
+    <!-- le composant correspondant à la route sera rendu ici -->
+    <router-view></router-view>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  computed: {
+    username() {
+      // Nous verrons ce que représente `params` dans un instant.
+      return this.$route.params.username
+    }
+  },
+  methods: {
+    goBack() {
+      window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
+    }
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.links a{
+  margin-left: 20px;
+  text-decoration: none; 
+  color :  #5c6161;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "vue-good-table/dist/vue-good-table.css";
+import VueGoodTable from 'vue-good-table'
+import VueRouter from 'vue-router'
+import Details from './Details.vue'
+import Films from './Films.vue'
+import Acteurs from './Acteurs.vue'
+import Personnage from './Personnage.vue'
+import AjoutPersonnage from './AjoutPersonnage.vue'
+import ModifierPersonnage from './ModifierPersonnage.vue'
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
+Vue.use(VueGoodTable)
+
+const routes = [
+  { path: '/films', component: Films },
+  { path: '/filmsDetails/:id',name :"filmsDetails" , component: Details },
+  { path: '/ajoutPersonnage',name :"ajoutPersonnage" , component: AjoutPersonnage},
+  { path: '/modifierPersonnage/:id',name :"modifierPersonnage" , component: ModifierPersonnage},
+  { path: '/personnage/:id',name :"personnage" , component: Personnage},
+  { path: '/acteurs', name: "acteurs", component: Acteurs}
+]
+const router = new VueRouter({
+  routes 
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
+
